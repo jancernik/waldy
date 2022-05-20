28355303c32bea855130c1bc0b05250d44b4cdb9
Jan Cernik <jancernik12@gmail.com>
2022-05-20 16:35:16 -0300

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    watchFiles: ["./src/*"],
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+});
